6f7bc9332a210a8c8642b258fe927c02b3f99472
Mrzhang <790774470@qq.com>
2020-09-10 11:07:09 +0800

    新增：登录界面简单实现

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default {
 
 <style>
 #app{
-  position: relative;
+  position: absolute;
   width: 100%;
   height: 100%;
 }
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,15 +1,82 @@
 <template>
-    <div>
-        登录界面
+    <div class="login-container">
+        <el-form ref="form" :rules="rules" :model="form" label-width="80px" class="login-form">
+            <h2 class="login-title">管理系统</h2>
+            <el-form-item label="用户名">
+                <el-input v-model="form.username" prop="username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="form.password" prop="password"></el-input>
+            </el-form-item>
+
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('form')">登录</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 <script>
 export default{
   name: 'login',
   data () {
-
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          {required: true, message: '用户名不能为空', trigger: 'blur'},
+          {min: 3, max: 10, message: '用户名3-5位', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '密码不能为空', trigger: 'blur'},
+          {min: 3, max: 10, message: '密码3-5位', trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate(valid => {
+        console.log(formName)
+        // console.log(valid) 验证通过为true，有一个不通过就是false
+        if (valid) {
+          // 通过的逻辑
+          if (this.form.username === 'admin' && this.form.password === '123456') {
+            this.$router.push('/main')
+          } else {
+            console.log('用户名密码错误')
+          }
+        } else {
+          console.log('验证失败')
+          return false
+        }
+      }, this)
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
+.login-form {
+  width: 350px;
+  margin: 160px auto; /* 上下间距160px，左右自动居中*/
+  background-color: RGB(255, 255, 255, 0.8); /* 透明背景色 */
+  padding: 30px;
+  border-radius: 20px; /* 圆角 */
+}
+
+/* 背景 */
+.login-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: black;
+}
+
+/* 标题 */
+.login-title {
+  color: #303133;
+  text-align: center;
+}
 </style>
